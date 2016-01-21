b5ac352ae0e167e2cec54be6f31afc2adf7547b3
Marisa Kanemoto <marisa.kanemoto@gmail.com>
2016-01-21 20:38:56 +0000

    Lab 3 including cute new images

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,4 +1,7 @@
 'use strict';
+// $("#div-id").click(functionToCall);
+// In an event handler, $(this) refers to      
+// the object that triggered the event   
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -10,9 +13,47 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("with one 's'");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
-
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(updateProject);
 }
+
+function updateProject(e) {
+	var projectID = $('#project').val();
+	$(projectID).animate({
+		width: $('#width').val()
+	});
+	var newText = $('#description').val();
+	$(projectID + " .project-description").text(newText);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the element that fired the event
+    var projectTitle = $(this).find("p").text();
+    //var jumbotronHeader = $("#jumbotron h1");
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(description).toggle();
+    }
+}
+
+
+/*
+function projectClick(e) { 
+	console.log("Project clicked");
+    // prevent the page from reloading      
+    e.preventDefault();   
+    $(this).css("background-color", "#7fff00");
+}
+*/
